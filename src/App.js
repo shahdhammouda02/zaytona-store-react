9dae9b00d3b15c854ebbce80c033765f9925c45d
zainaalbakri <zaina2002albakri@gmail.com>
2025-01-05 02:15:16 +0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Hero from "./hero/hero";
+import PrimarySearchAppBar from "../src/components/navbar/nav";
+const theme = createTheme({
+  typography: {
+    fontFamily: "Cairo, Arial, Tahoma, sans-serif",
+  },
+});
+function App() {
+  return (
+    <ThemeProvider className="App" theme={theme}>
+      <PrimarySearchAppBar />
+      <Hero />
+    </ThemeProvider>
+  );
+}
+
+export default App;
